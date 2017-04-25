9842e0c061191d988591dba0a18cdb70cdf7b262
jroberto <jroberto@jr>
2017-04-25 14:06:17 +0200

    hash map casi completo

--- src/Main/MainMapa.java
+++ src/Main/MainMapa.java
@@ -14,14 +14,15 @@ public class MainMapa {
 	        
 	        
 		 Map <Integer,Movil> mapa = new HashMap<Integer,Movil>();
-	        Movil m1 = new Movil("color","algo","algo",40,"ds");
-	        mapa.put(1,m1);
-	        Movil m2= new Movil("color","algo","algo",40,"sdfa");
-	        mapa.put(2,m2);
-	        Movil m3 = new Movil("color","algo","algo",40,"sdafad");
-	        mapa.put(3,m3);
-	        Movil m4 = new Movil("co","ago","algo",20,"sdfa");
-	        mapa.put(4,m4);
+		 	Movil movil1 = new Movil("Iphone", "7 plus", "Oro", 909, "A10");
+			Movil movil2 = new Movil("Samsung", "S8 plus", "Negro", 909, "Exynos 8895 o Qualcomm Snapdragon 835 ");
+			Movil movil3 = new Movil("Sony", "Xperia A1 Ultra", "Negro", 456, "MediaTek Helio P20");
+			Movil movil4 = new Movil("Xiaomi", "Mi 5s plus", "Negro", 410, "Qualcomm Snapdragon 821");
+//añadimos los moviles al mapa
+	        mapa.put(1,movil1);
+	        mapa.put(2,movil2);
+	        mapa.put(3,movil3);
+	        mapa.put(4,movil4);
 	        
 	        
 System.out.println(mapa.remove(3));//borra el movil con esa clave
