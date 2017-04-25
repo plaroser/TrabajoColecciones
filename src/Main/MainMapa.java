fbf91948f35da81ffbcb8c47ef2386289a4a2bfa
jroberto <jroberto@jr>
2017-04-25 14:03:30 +0200

    hashmap terminada

--- src/Main/MainMapa.java
+++ src/Main/MainMapa.java
@@ -9,25 +9,34 @@ public class MainMapa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Variables para Metodos
+		 
+	        
+	        
 		 Map <Integer,Movil> mapa = new HashMap<Integer,Movil>();
-	        Movil p = new Movil("color","algo","algo",40,"ds");
-	        mapa.put(4,p);
-	        Movil a = new Movil("color","algo","algo",40,"sdfa");
-	        mapa.put(1,a);
-	        Movil e = new Movil("color","algo","algo",40,"sdafad");
-	        mapa.put(2,e);
-	        Movil f = new Movil("co","ago","algo",20,"sdfa");
-	        mapa.put(3,f);
-	        
-	        System.out.println(mapa.toString());
-	        
-	        int codigo=10;
-	        String modelo="";
-	        String marca="";
-	        String Color="";
-	        String Pr
+	        Movil m1 = new Movil("color","algo","algo",40,"ds");
+	        mapa.put(1,m1);
+	        Movil m2= new Movil("color","algo","algo",40,"sdfa");
+	        mapa.put(2,m2);
+	        Movil m3 = new Movil("color","algo","algo",40,"sdafad");
+	        mapa.put(3,m3);
+	        Movil m4 = new Movil("co","ago","algo",20,"sdfa");
+	        mapa.put(4,m4);
 	        
 	        
+System.out.println(mapa.remove(3));//borra el movil con esa clave
+System.out.println( mapa.get(4));//devuelve el movil con esa clave
+System.out.println(mapa.containsKey(2));//devuelve true si el mapa contiene esa clave
+System.out.println(mapa.values());//devuelve el mapa como una lista
+System.out.println( mapa.size());//devuelve el tamao del mapa
+
+        
+	       
+	        //mapa.clear();
+	        
+       
+	}
+	
 	}
 
-}
+
