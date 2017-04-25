740995cedbe1ad65343f85e354c61ee3437feb03
Alberto <Alberto@192.168.1.126>
2017-04-25 12:58:47 +0200

    Modelo completado

--- src/Models/Movil.java
+++ src/Models/Movil.java
@@ -1,5 +1,59 @@
 package Models;
 
 public class Movil {
+	
+	//atributos
+	private String marca;
+	private String modelo;
+	private String color;
+	private int precio;
+	
+	//Constructor
+	public Movil(String marca, String modelo, String color, int precio) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.color = color;
+		this.precio = precio;
+	}
 
+	//Metodos
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	@Override
+	public String toString() {
+		return "Movil [marca=" + marca + ", modelo=" + modelo + ", color=" + color + ", precio=" + precio + " €]";
+	}
+	
+	
+	
 }
