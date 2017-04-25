e7956e512661181f29a56b1af528f0acc5742a1d
Alberto <Alberto@192.168.1.126>
2017-04-25 14:25:16 +0200

    mainset acabado

--- src/Main/MainSet.java
+++ src/Main/MainSet.java
@@ -16,29 +16,29 @@ public class MainSet {
 
 		HashSet<Movil> listaMovil = new HashSet<>();
 		TreeSet<Movil> listaMoviles = new TreeSet<>((Movil o1, Movil o2) -> {//Utilizamos expresion lambda para ahorrarnos crear una clase comparator
-			if ((o1.getPrecio() - o2.getPrecio()) != 0)
-				return o1.getPrecio() - o2.getPrecio();
+			if ((o1.getPrecio() - o2.getPrecio()) != 0)//Ordenamos por precio, y si son iguales se
+				return o1.getPrecio() - o2.getPrecio();//ordenan por modelo
 			else
 				return o1.getModelo().compareTo(o2.getModelo());
 		});
 
-		listaMovil.add(movil1);
+		listaMovil.add(movil1);//Añadimos moviles a la lista HashSet
 		listaMovil.add(movil2);
 		listaMovil.add(movil3);
 		listaMovil.add(movil4);
 
-		listaMoviles.add(movil1);
+		listaMoviles.add(movil1);//Añadimos moviles a la lista TreeSet
 		listaMoviles.add(movil2);
 		listaMoviles.add(movil3);
 		listaMoviles.add(movil4);
 
-		for (Movil i : listaMovil) {
+		for (Movil i : listaMovil) {//Mostramos la lista HashSet
 			System.out.println(i);
 
 		}
 
 		System.out.println("");
-		listaMovil.remove(movil1);
+		listaMovil.remove(movil1);//Borramos moviles de la lista HashSet
 		listaMovil.remove(movil3);
 
 		for (Movil i : listaMovil) {
@@ -48,11 +48,11 @@ public class MainSet {
 
 		System.out.println("");
 
-		if (listaMovil.isEmpty()) {
+		if (listaMovil.isEmpty()) {//Miramos si la lista esta vacia
 			System.out.println("No hay moviles en este momento.");
 		}
 
-		else
+		else//Sino esta vacia
 			System.out.println("Estos son los moviles actules:");
 		for (Movil i : listaMovil) {
 			System.out.println(i);
@@ -60,9 +60,10 @@ public class MainSet {
 		}
 
 		System.out.println("");
+		//Mostramos el primer movil de la lista TreeSet
 		System.out.println("Mostramos el primer movil:"+listaMoviles.first());
-		listaMoviles.first();
 
+		//Mostramos el ultimo movil de la lista TreeSet
 		System.out.println("Mostramos el ultimo movil:"+listaMoviles.last());
 		
 
