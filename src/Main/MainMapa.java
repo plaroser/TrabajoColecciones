bc97673a2c0f8eca35194db6ed005e99f2dbccc3
jroberto <jroberto@jr>
2017-04-25 13:21:47 +0200

    main mapa

--- src/Main/MainMapa.java
+++ src/Main/MainMapa.java
@@ -9,18 +9,25 @@ public class MainMapa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 Map <Integer,Movil> mp = new HashMap<Integer,Movil>();
+		 Map <Integer,Movil> mapa = new HashMap<Integer,Movil>();
 	        Movil p = new Movil("color","algo","algo",40,"ds");
-	        mp.put(4,p);
+	        mapa.put(4,p);
 	        Movil a = new Movil("color","algo","algo",40,"sdfa");
-	        mp.put(1,a);
+	        mapa.put(1,a);
 	        Movil e = new Movil("color","algo","algo",40,"sdafad");
-	        mp.put(2,e);
+	        mapa.put(2,e);
 	        Movil f = new Movil("co","ago","algo",20,"sdfa");
-	        mp.put(3,f);
+	        mapa.put(3,f);
 	        
-	        System.out.println(mp.toString());
-	        System.out.println(mp.put(3,f));
+	        System.out.println(mapa.toString());
+	        
+	        int codigo=10;
+	        String modelo="";
+	        String marca="";
+	        String Color="";
+	        String Pr
+	        
+	        
 	}
 
 }
