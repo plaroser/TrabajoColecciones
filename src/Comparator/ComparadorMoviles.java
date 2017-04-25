4617a9a5eb1589c4389f732ac472da0a3bda33f9
sergio <sergio_pla_11@hotmail.com>
2017-04-25 13:55:48 +0200

    Comparador corregido

--- src/Comparator/ComparadorMoviles.java
+++ src/Comparator/ComparadorMoviles.java
@@ -8,7 +8,7 @@ public class ComparadorMoviles implements Comparator<Movil> {
 
 	@Override
 	public int compare(Movil o1, Movil o2) {
-		if ((o1.getPrecio() - o2.getPrecio()) == 0)
+		if ((o1.getPrecio() - o2.getPrecio()) != 0)
 			return o1.getPrecio() - o2.getPrecio();
 		else
 			return o1.getModelo().compareTo(o2.getModelo());
--- src/Main/MainComparator.java
+++ src/Main/MainComparator.java
@@ -22,7 +22,7 @@ public class MainComparator {
 
 		// Crear el treeSet con el comparador en expresion landa
 		TreeSet<Movil> ListaMovilesLanda = new TreeSet<>((Movil o1, Movil o2) -> {
-			if ((o1.getPrecio() - o2.getPrecio()) == 0)
+			if ((o1.getPrecio() - o2.getPrecio()) != 0)
 				return o1.getPrecio() - o2.getPrecio();
 			else
 				return o1.getModelo().compareTo(o2.getModelo());
